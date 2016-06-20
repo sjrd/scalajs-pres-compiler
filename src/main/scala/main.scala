8f89ff8296c7e7de7babe483fac2d312fed8b1dd
Roger Vion <roger.vion@epfl.ch>
2016-06-20 11:21:47 +0200

    A popup, but failure to remove the event listener... works in pure javascript, but how to properly translate in Scala.js ?

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,42 +8,69 @@ import scala.scalajs.js
 import js.Dynamic.{global => g}
 import js.annotation.JSExport
 import scala.annotation.meta.field
-import org.scalajs.dom.document
+import org.scalajs.dom._
 
 object ScalaJSAutocompleter extends JSApp {
-	println("Initializing...")
-	@JSExport
-	val fileLoader = new Classpath
+  println("Initializing...")
+  @JSExport
+  val fileLoader = new Classpath
 
-	@JSExport
-	def askAutocompletion(editor: js.Dynamic) {
+  def getDocumentCoordinates(element: raw.Element): Map[String, Double] = {
+    val boundingRect = element.getBoundingClientRect();
+    Map("left" -> (boundingRect.left.asInstanceOf[Double] + window.asInstanceOf[js.Dynamic].scrollX.asInstanceOf[Double]), "top" -> (boundingRect.top.asInstanceOf[Double] + window.asInstanceOf[js.Dynamic].scrollY.asInstanceOf[Double]))
+  }
+
+  @JSExport
+  def askAutocompletion(editor: js.Dynamic) {
     val code = editor.getValue().asInstanceOf[String]
     /* The row and column in the JS object returned by getCursorPosition
      * start at (0, 0) for a cursor positioned completely on the left on
      * the top line
-		 */
+     */
     val row = editor.getCursorPosition().row.asInstanceOf[Int]
-  	val column = editor.getCursorPosition().column.asInstanceOf[Int]
+    val column = editor.getCursorPosition().column.asInstanceOf[Int]
     val intOffset = column + code.split("\n").take(row).map(_.length + 1).sum
     val flag = if(code.take(intOffset).endsWith(".")) "member" else "scope"
-		try {
-			val compiler = new Compiler(fileLoader)
-			val future = compiler.autocomplete(code, flag, intOffset)
-			future onComplete {
-				case Success(possibleCompletions) => {
-					var listItems = new StringBuilder
-					for (c <- possibleCompletions) {
-						listItems ++= s"<li>${c._2}${c._1}</li>"
-					}
-					document.getElementById("completions").innerHTML = listItems.toString
-					println("autocompletion done")
-				}
-				case Failure(t) => t.printStackTrace
-			}
-		} catch {
-			case th: Throwable => th.printStackTrace
-		}
-	}
+    try {
+      val compiler = new Compiler(fileLoader)
+      val future = compiler.autocomplete(code, flag, intOffset)
+      future onComplete {
+        case Success(possibleCompletions) => {
+          var listItems = new StringBuilder
+          for (c <- possibleCompletions) {
+            listItems ++= s"<li>${c._2}${c._1}</li>"
+          }
 
-	def main(): Unit = { }
+          var firstRow = document.querySelector(".container-fluid .row:first-child")
+          var cursorElem = document.querySelector(".ace_cursor") // or ".ace_text-input (1 px less for left...)
+          var cursorCoords = getDocumentCoordinates(cursorElem)
+          var firstRowCoords = getDocumentCoordinates(firstRow)
+
+          var list = document.createElement("ul")
+          list.setAttribute("id", "completion-list")
+          list.asInstanceOf[js.Dynamic].style.position = "absolute";
+          list.asInstanceOf[js.Dynamic].style.left = cursorCoords("left") - firstRowCoords("left") + "px";
+          list.asInstanceOf[js.Dynamic].style.top = cursorCoords("top") - firstRowCoords("top") + "px";
+          list.innerHTML = listItems.toString
+
+          def closeFunction(event: js.Dynamic) {
+            if (event.keyCode.asInstanceOf[Int] == 27) {
+              firstRow.removeChild(list);
+              document.removeEventListener("keyup", closeFunction _); // this doesn't work, but it works in pure JavaScript
+              // Furthermore, Scala.js does not support arguments.callee, so we can't use that
+            }
+          }
+
+          document.addEventListener("keyup", closeFunction _);
+          document.querySelector(".container-fluid .row:first-child").appendChild(list);
+          println("autocompletion done")
+        }
+        case Failure(t) => t.printStackTrace
+      }
+    } catch {
+      case th: Throwable => th.printStackTrace
+    }
+  }
+
+  def main(): Unit = { }
 }
